292e387da6c1ee76e77a4678b1bdb3bb2df2bf08
urbanno1 <eduurbanus42@gmail.com>
2020-08-30 22:06:54 -0400

    added application.yml and bootstrap.yml to webshopserver

--- WebShopServer/src/main/java/com/monycell/edu/webshopserver/WebShopServerApplication.java
+++ WebShopServer/src/main/java/com/monycell/edu/webshopserver/WebShopServerApplication.java
@@ -2,8 +2,10 @@ package com.monycell.edu.webshopserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WebShopServerApplication {
 
     public static void main(String[] args) {
